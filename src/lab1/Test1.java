ddf4181d9ef7908bacb0b19b22518c7a89c93cec
M100118\student <fg123a@naver.com>
2017-08-21 16:32:21 +0900

    첫번째버전

---
+++ src/lab1/Test1.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Test1 class(lab1)");
+	}
+
+}
---
+++ src/lab2/Test1.java
@@ -0,0 +1,7 @@
+package lab2;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Test1 class(lab2)");
+	}
+}
